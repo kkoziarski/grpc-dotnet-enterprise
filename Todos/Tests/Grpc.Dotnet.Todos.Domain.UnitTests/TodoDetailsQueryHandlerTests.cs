be3a614c78cfc3616069a94b4803ac8f36a00043
Krzysztof Koziarski <krzyhook@gmail.com>
2020-02-15 11:11:18 +0100

    unit test TodoDetailsQueryHandler

---
+++ Todos/Tests/Grpc.Dotnet.Todos.Domain.UnitTests/TodoDetailsQueryHandlerTests.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Grpc.Core;
+using Grpc.Dotnet.Permissions.V1;
+using Grpc.Dotnet.Shared.Helpers.UnitTests.Helpers;
+using Grpc.Dotnet.Todos.Domain.Domain;
+using Grpc.Dotnet.Todos.Domain.Queries;
+using Grpc.Dotnet.Todos.Domain.QueryHandlers;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Grpc.Dotnet.Todos.Domain.UnitTests
+{
+    public class TodoDetailsQueryHandlerTests
+    {
+        private RpcClientMock<PermissionsService.PermissionsServiceClient> rpcPermissionsMock;
+        private IMapper mapper;
+
+        public TodoDetailsQueryHandlerTests()
+        {
+
+            this.rpcPermissionsMock = new RpcClientMock<PermissionsService.PermissionsServiceClient>();
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AutoMapperProfile>();
+            });
+            this.mapper = new Mapper(configuration);
+        }
+
+        [Fact]
+        public async Task Handle_WhenUserDoesNotHavePermission_ShouldThrowException()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var request = new TodoDetailsQuery { Id = 1, UserId = userId };
+            var rpcResponse = new IsUserAllowedResponse { IsAllowed = false };
+            rpcPermissionsMock.ClientMock.Setup(x => x.IsUserAllowed(It.IsAny<IsUserAllowedRequest>(), It.IsAny<CallOptions>())).Returns(rpcResponse);
+
+            var dbOptions = new DbContextOptionsBuilder<TodoDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            using (var context = new TodoDbContext(dbOptions))
+            {
+                // Act
+                var sut = new TodoDetailsQueryHandler(context, mapper, rpcPermissionsMock.ServiceClient);
+                try
+                {
+                    var result = await sut.Handle(request, new CancellationToken(false));
+                    Assert.True(false, "Should throw exception");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    // Assert
+                    Assert.NotNull(ex);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Handle_WhenUserHasPermission_ShouldNotThrowException()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var request = new TodoDetailsQuery { Id = 1, UserId = userId };
+            var rpcResponse = new IsUserAllowedResponse { IsAllowed = true };
+            rpcPermissionsMock.ClientMock.Setup(x => x.IsUserAllowed(It.IsAny<IsUserAllowedRequest>(), It.IsAny<CallOptions>())).Returns(rpcResponse);
+
+            var dbOptions = new DbContextOptionsBuilder<TodoDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            using (var context = new TodoDbContext(dbOptions))
+            {
+                var sut = new TodoDetailsQueryHandler(context, mapper, rpcPermissionsMock.ServiceClient);
+
+                // Act
+                var result = await sut.Handle(request, new CancellationToken(false));
+            }
+        }
+
+        [Fact]
+        public async Task Handle_WhenUserHasPermission_ShouldReturnAllTodos()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var request = new TodoDetailsQuery { Id = 2, UserId = userId };
+            var rpcResponse = new IsUserAllowedResponse { IsAllowed = true };
+            rpcPermissionsMock.ClientMock.Setup(x => x.IsUserAllowed(It.IsAny<IsUserAllowedRequest>(), It.IsAny<CallOptions>())).Returns(rpcResponse);
+
+            var dbOptions = new DbContextOptionsBuilder<TodoDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            using (var context = new TodoDbContext(dbOptions))
+            {
+                context.Todos.Add(new Todo { Id = 1, Name = "todo 1" });
+                context.Todos.Add(new Todo { Id = 2, Name = "todo 2" });
+                context.SaveChanges();
+            }
+
+            using (var context = new TodoDbContext(dbOptions))
+            {
+                var sut = new TodoDetailsQueryHandler(context, mapper, rpcPermissionsMock.ServiceClient);
+                // Act
+                var result = await sut.Handle(request, new CancellationToken(false));
+
+                result.Id.ShouldBe(2);
+                result.Name.ShouldBe("todo 2");
+            }
+        }
+    }
+}
